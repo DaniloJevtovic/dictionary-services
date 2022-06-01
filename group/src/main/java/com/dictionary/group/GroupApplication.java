2440d83e8f9611dc016886b750813951ab852343
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2022-06-01 20:38:18 +0200

    anotacija za open feign klijente

--- group/src/main/java/com/dictionary/group/GroupApplication.java
+++ group/src/main/java/com/dictionary/group/GroupApplication.java
@@ -3,9 +3,12 @@ package com.dictionary.group;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.dictionary.group"})
 @EnableEurekaClient
+@EnableFeignClients(basePackages = "com.dictionary.clients")
 public class GroupApplication {
     public static void main(String[] args) {
         SpringApplication.run(GroupApplication.class, args);
