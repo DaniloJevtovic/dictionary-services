423f8340eed98374cdfd009d9895eeeeee58aa63
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2023-02-21 13:17:44 +0100

    izmjene u servisu za recenice

--- sentence/src/main/java/com/dictionary/sentence/SentenceApplication.java
+++ sentence/src/main/java/com/dictionary/sentence/SentenceApplication.java
@@ -2,11 +2,9 @@ package com.dictionary.sentence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {"com.dictionary.sentence"})
-@EnableEurekaClient
 @EnableFeignClients(basePackages = "com.dictionary.clients")
 public class SentenceApplication {
     public static void main(String[] args) {
